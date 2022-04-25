cd9ba5aea17b3021b0fcdd60b04e59056f9c1a71
shyoon63 <shyoon63@e-mirim.hs.kr>
2022-04-25 14:43:06 +0900

    views BookmarkDeleteView

--- bookmark/views.py
+++ bookmark/views.py
@@ -1,5 +1,5 @@
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView, DetailView, UpdateView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 
 from bookmark.models import Bookmark
 
@@ -25,3 +25,8 @@ class BookmarkUpdateView(UpdateView):
     fields = ['name', 'url']    #'__all__'
     template_name_suffix = '_update'    #bookmark_update.html
     # success_url = reverse_lazy('bookmark:list')   #success_url 없으면 model의 get_absolute_url() 호출
+
+
+class BookmarkDeleteView(DeleteView):
+    model = Bookmark
+    success_url = reverse_lazy('bookmark:list')
