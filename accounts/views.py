665ae2a34290de4ba435c4ad4f3c85fed0f718ad
shyoon63 <shyoon63@e-mirim.hs.kr>
2022-06-13 14:27:37 +0900

    Fixed 회원가입 시 조건을 만족하지 못하면 그릴 페이지 없음 문제

--- accounts/views.py
+++ accounts/views.py
@@ -12,7 +12,7 @@ def register(request):
             return render(request, 'accounts/register_done.html', {'profile': profile})
     else:  # 처음 빈 폼 화면
         form = RegisterForm()
-        return render(request, 'accounts/register.html', {'form': form})
+    return render(request, 'accounts/register.html', {'form': form})
 
 
 def my_login(request):
